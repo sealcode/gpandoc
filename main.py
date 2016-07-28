2eb454c9be06deaee2e90030b4bc1d03f862d43c
Jerrykx <jaroslaw.k.wieczorek@student.put.poznan.pl>
2016-07-28 20:10:47 +0200

    Add resouces file

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import sys
-from PyQt5 import *
 sys.path.insert(0, 'ui/')
 import mainwindow_ui
 
 from PyQt5 import QtWidgets
 
+def load_resources():
+    pass
+
+def add_files():
+    pass
+
+
 app = QtWidgets.QApplication(sys.argv)
 window = mainwindow_ui.UI_MainWindow()
+
 
 sys.exit(app.exec_())
